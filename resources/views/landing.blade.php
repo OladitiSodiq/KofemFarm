30b92a0b7f97e7d55586b74c7824e8e9be5f6cb0
oladitisodiq <oladitisodiq@gmail.com>
2024-08-12 10:40:00 +0100

    meet our team removed to fix

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -480,7 +480,7 @@
         
     <div class="container-fluid bg-dark text-white py-4">
         <div class="container text-center">
-            <p class="mb-0">&copy; <a class="text-secondary fw-bold" href="#">Kofem </a>. All Rights Reserved. Designed by  Atom's Hub Technologies</a></p>
+            <p class="mb-0">&copy; <a class="text-secondary fw-bold" href="#">Kofem </a>. All Rights Reserved. Designed by <a class="text-primary fw-bold" href="https://oladitisodiq.github.io/">Atom's Hub Technologies </a></p>
         </div>
     </div>
     <!-- Footer End -->
